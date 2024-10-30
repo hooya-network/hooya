b73500df7bf7bc19ff2962c1bd5696f927331d02
Wesley Coakley <wesley@skip.money>
2024-10-30 02:05:51 -0400

    use flakes

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "hooya";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        isDarwin = pkgs.stdenv.isDarwin;
+      in {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            rust-bin.stable."1.82.0".default
+            ffmpeg
+            git
+            protobuf
+            gtk4
+          ] ++ lib.optional isDarwin (with darwin.apple_sdk.frameworks; [ Security CoreServices ]);
+        };
+      }
+    );
+}
+
