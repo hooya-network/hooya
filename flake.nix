f3ec7b08e3ab97696db9affdafadacb853cf1e96
Wesley Coakley <w@wesl.ee>
2025-07-28 00:57:46 -0400

    build workflow

--- flake.nix
+++ flake.nix
@@ -16,6 +16,26 @@
         };
         isDarwin = pkgs.stdenv.isDarwin;
       in {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "hooya";
+          version = "0.1.2";
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            protobuf
+          ];
+
+          buildInputs = with pkgs; [
+            openssl
+            ffmpeg
+          ] ++ lib.optionals isDarwin (with darwin.apple_sdk.frameworks; [ Security CoreServices ]);
+        };
+
         devShells.default = with pkgs; mkShell {
           buildInputs = [
             (rust-bin.stable."1.88.0".default.override {
